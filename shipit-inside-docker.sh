ae7bcffc9a065c9450755c4a84e2ded3ad51db16
Fabian Stegemann <fst@zetaron.de>
2016-11-01 21:52:23 +0100

    Improvements for docker stack deployments

--- shipit-inside-docker.sh
+++ shipit-inside-docker.sh
@@ -2,7 +2,11 @@
 
 if [ ! -f /usr/src/shipit/.firstrun ]
 then
-    sleep 3 # wait for postgres to be started
+    # wait for the redis server to be up
+    while ! ping -c1 redis &>/dev/null; do :; done
+    # wait for the postgres server to be up
+    while ! ping -c1 postgres &>/dev/null; do :; done
+    sleep 3 # give it another 3 seconds to completly boot
     bundle exec rake railties:install:migrations db:create db:migrate
     DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:setup
 
